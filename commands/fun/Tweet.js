bf9f40caa11f23258f284629bfa0939d2b8c507c
Natsukoo1 <151683968+Natsukoo1@users.noreply.github.com>
2025-07-18 01:12:55 +0200

    Update Tweet.js

--- commands/fun/Tweet.js
+++ commands/fun/Tweet.js
@@ -61,8 +61,8 @@ module.exports = {
             avatarImage.mask(mask, 0, 0);
 
             // Descendre l'image d'environ 2cm (~75 pixels)
-            const avatarX = 77;
-            const avatarY = 29 + 75;
+            const avatarX = 76;
+            const avatarY = 31 + 75;
 
             tweetImage.composite(avatarImage, avatarX, avatarY);
 
